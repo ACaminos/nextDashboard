ae92ade7389ad20ae5269f5a7ce7c5c731e2d533
Ariel <caminosariel@gmail.com>
2024-02-20 15:59:37 -0300

    add new page, use .map to show another category

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image"
-import { IoLogoReact } from "react-icons/io5"
+import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5"
 import { SidebarMenuItem } from "./SidebarMenuItem"
+
+const menuItems = [
+    {
+        path : '/dashboard/main',
+        icon : <IoBrowsersOutline size={40} />,
+        title : 'Dashboard',
+        subTitle : 'Visualizacion'
+    },
+    {
+        path : '/dashboard/counter',
+        icon : <IoCalculator size={40} />,
+        title : 'Counter',
+        subTitle : 'Contador Client'
+    }
+]
 
 export const Sidebar = () => {
   return (
@@ -34,19 +49,16 @@ export const Sidebar = () => {
     </div>
     <div id="nav" className="w-full px-6">
 
-        {/* <SidebarMenuItem icon={} path="" subTitle="" title="" key={} /> */}
-
-        <a href="#" className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 hover:bg-white/5 transition ease-linear duration-150">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-                  </svg>
-            </div>
-            <div className="flex flex-col">
-                <span className="text-lg text-slate-300 font-bold leading-5">Counter</span>
-                <span className="text-sm text-slate-500 hidden md:block">Estado Local</span>
-            </div>
-        </a>
+        { menuItems.map( menuItem => (
+            <SidebarMenuItem
+                icon={ menuItem.icon }
+                path={ menuItem.path }
+                subTitle={ menuItem.subTitle }
+                title={ menuItem.title }
+                key={menuItem.path} />
+                )
+            )
+        }
     </div>
  </div>
   )
