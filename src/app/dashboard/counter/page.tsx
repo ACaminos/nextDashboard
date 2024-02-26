81e187eaaaa4ff3486ac372b130334aa56918cb0
Ariel <caminosariel@gmail.com>
2024-02-26 16:48:09 -0300

    add useState

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,45 @@
+'use client'
+import { useState } from "react";
 
 export default function CounterPage() {
+
+  const [ num, setNum ] = useState(10)
+
+  const sum = () => { setNum( num + 1) }
+  const supr = () => { setNum( num - 1) }
+
   return (
-    <div>
-      <h1>Hello Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">{ num }</span>
+      <div className="flex">
+        <button className="
+          flex
+          items-center
+          justify-center
+          p-2
+          rounded-xl
+          bg-gray-900
+          text-white
+          hover:bg=gray-600
+          transition-all
+          w-[100px]
+          mr-2"
+          onClick={ sum }> + 1 </button>
+        <button className="
+          flex
+          items-center
+          justify-center
+          p-2
+          rounded-xl
+          bg-gray-900
+          text-white
+          hover:bg=gray-600
+          transition-all
+          w-[100px]
+          mr-2"
+          onClick={ supr }> - 1 </button>
+      </div>
     </div>
   );
 }
